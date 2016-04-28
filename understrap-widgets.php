82e20d6a7c0e4ab63c685e50cc73a498b392661f
Diego Versiani <diegoversiani@gmail.com>
2016-04-27 21:35:00 -0300

    Initial Commit.

---
+++ understrap-widgets.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Understrap Widgets
+ * Plugin URI: http://github.com/dvlopes/undestrap-widgets
+ * Description: Adds commom website sections as widgets to be used as building blocks for Understrap Theme or any Bootstrap theme.
+ * Version: 0.0.1
+ * Author: Diego Versiani
+ * Author URI: http://dvlopes.github.io/
+ * License: GPL2
+ */
