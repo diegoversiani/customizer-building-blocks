b5605bd70aaa5d8493e2b7bfe4fa24a40f918ed9
Diego Versiani <diegoversiani@gmail.com>
2016-04-28 19:58:17 -0300

    Section Begin improvements.

--- widgets/understrap_section_begin_widget.php
+++ widgets/understrap_section_begin_widget.php
@@ -14,6 +14,11 @@ class UnderstrapSectionBegin_Widget extends WP_Widget {
 
     $container_class = ( $instance['container_fluid'] ? 'container-fluid' : 'container' );
 
+    if ( ! empty( $instance['title_tag'] ) ) {
+      $args['before_title'] = '<' . esc_attr( $instance['title_tag'] ) . ' class="featured-title">';
+      $args['after_title'] = '</' . esc_attr( $instance['title_tag'] ) . '>';
+    }
+
     echo $args['before_widget'];
 
     ?> 
@@ -30,6 +35,7 @@ class UnderstrapSectionBegin_Widget extends WP_Widget {
 
   public function form( $instance ) {
     $title = ! empty( $instance['title'] ) ? $instance['title'] : '';
+    $title_tag = ! empty( $instance['title_tag'] ) ? $instance['title_tag'] : '';
     $container_fluid = $instance[ 'container_fluid' ] ? 'true' : 'false';
     $css_class = ! empty( $instance['css_class'] ) ? $instance['css_class'] : '';
     ?>
@@ -38,6 +44,21 @@ class UnderstrapSectionBegin_Widget extends WP_Widget {
     <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'understrap_widgets' ); ?></label> 
     <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php esc_attr_e( $title ); ?>">
     </p>
+
+    <p>
+    <label><?php _e( 'Title Tags:', 'understrap_widgets' ); ?></label>
+    <br>
+    <select class="widefat" id="<?php echo $this->get_field_id( 'title_tag' ); ?>" name="<?php echo $this->get_field_name( 'title_tag' ); ?>">
+      <option value="" <?php esc_attr_e( $title_tag == '' ? 'selected' : '' ); ?>><?php _e( '-- None --', 'understrap_widgets' ); ?></option>
+      <option value="h1" <?php esc_attr_e( $title_tag == 'h1' ? 'selected' : '' ); ?>><?php _e( 'h1', 'understrap_widgets' ); ?></option>
+      <option value="h2" <?php esc_attr_e( $title_tag == 'h2' ? 'selected' : '' ); ?>><?php _e( 'h2', 'understrap_widgets' ); ?></option>
+      <option value="h3" <?php esc_attr_e( $title_tag == 'h3' ? 'selected' : '' ); ?>><?php _e( 'h3', 'understrap_widgets' ); ?></option>
+      <option value="h4" <?php esc_attr_e( $title_tag == 'h4' ? 'selected' : '' ); ?>><?php _e( 'h4', 'understrap_widgets' ); ?></option>
+      <option value="h5" <?php esc_attr_e( $title_tag == 'h5' ? 'selected' : '' ); ?>><?php _e( 'h5', 'understrap_widgets' ); ?></option>
+      <option value="h6" <?php esc_attr_e( $title_tag == 'h6' ? 'selected' : '' ); ?>><?php _e( 'h6', 'understrap_widgets' ); ?></option>
+      <option value="span" <?php esc_attr_e( $title_tag == 'span' ? 'selected' : '' ); ?>><?php _e( 'span', 'understrap_widgets' ); ?></option>
+    </select>
+    <small><?php _e( 'Apply only for the section title, not the widgets inside it.', 'understrap_widgets' ); ?></small>
 
     <p>
     <input class="checkbox" type="checkbox" <?php checked( $instance[ 'container_fluid' ], 'on' ); ?> id="<?php echo $this->get_field_id( 'container_fluid' ); ?>" name="<?php echo $this->get_field_name( 'container_fluid' ); ?>" />
@@ -56,6 +77,10 @@ class UnderstrapSectionBegin_Widget extends WP_Widget {
   public function update( $new_instance, $old_instance ) {
     $instance = array();
     $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+    
+    $title_tag_allowed = array('h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span');
+    $instance['title_tag'] = ( in_array($new_instance['title_tag'], $title_tag_allowed) ) ? $new_instance['title_tag'] : '';
+    
     $instance['container_fluid'] = $new_instance['container_fluid'];
     // TODO: Sanitize css classes for allowed characters
     $instance['css_class'] = ( ! empty( $new_instance['css_class'] ) ) ? sanitize_text_field( $new_instance['css_class'] ) : '';
