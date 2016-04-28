9bf6256d80a1d294eb58907e50b0811b5544d87f
Diego Versiani <diegoversiani@gmail.com>
2016-04-28 18:56:50 -0300

    CTA Widget improvements.

--- widgets/understrap_cta_widget.php
+++ widgets/understrap_cta_widget.php
@@ -1,12 +1,7 @@
 <?php 
-/**
-* Adds UnderstrapCTA_Widget widget.
-*/
+
 class UnderstrapCTA_Widget extends WP_Widget {
 
-  /**
-   * Sets up the widgets name etc
-   */
   public function __construct() {
     $widget_ops = array( 
       'classname' => 'understrap_cta_widget',
@@ -15,61 +10,39 @@ class UnderstrapCTA_Widget extends WP_Widget {
     parent::__construct( 'understrap_cta_widget', __( 'Understrap CTA/Call Out', 'understrap_widgets'), $widget_ops );
   }
 
-  /**
-   * Front-end display of widget.
-   *
-   * @see UnderstrapCTA_Widget::widget()
-   *
-   * @param array $args     Widget arguments.
-   * @param array $instance Saved values from database.
-   */
   public function widget( $args, $instance ) {
+
+    $button_css_class = ( $instance['button_css_class'] ? $instance['button_css_class'] : 'btn-default' );
+
     echo $args['before_widget'];
-    if ( ! empty( $instance['title'] ) ) {
-      echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
-    }
 
-    $container_class = ( $instance['container_fluid'] ? 'container-fluid' : 'container' );
-    
     ?>
-    <div class="wrapper wrapper-cta <?php  esc_attr_e( $instance['css_class'] ); ?>">
-      <div class="<?php esc_attr_e( $container_class ); ?>">
-        <div class="row">
-          <div class="col-xs-12 cta-content">
-            <?php 
-            if ( ! empty( $instance['title'] ) ) {
-              echo $args['before_title'] . esc_html( $instance['title'] ) . $args['after_title'];
-            }
-            ?>
-          
-            <?php echo wp_kses_post( $instance['text'] ); ?>
-            <?php if ( ! empty( $instance['button_text'] ) ) : ?>
-              <a href="<?php esc_url_e( $instance['button_href'] ); ?>" class="btn <?php esc_attr_e( $instance['button_css_class'] ); ?>"><?php esc_html_e( $instance['button_text'] ); ?></a>
-            <?php endif; ?>
-          </div>
-        </div>
-      </div>
-    </div>
-    <?php
+      <div class="understrap-cta <?php esc_attr_e( $instance['css_class'] ); ?>">
+        <?php 
+        if ( ! empty( $instance['title'] ) ) {
+          echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+        }
+        ?>
 
+        <p><?php echo wp_kses_post( $instance['text'] ); ?></p>
+
+        <?php if ( ! empty( $instance['button_text'] ) ) : ?>
+          <a href="<?php echo esc_url( $instance['button_href'] ); ?>" class="btn <?php esc_attr_e( $button_css_class ); ?>"><?php esc_html_e( $instance['button_text'] ); ?></a>
+        <?php endif; ?>
+
+      </div>
+
+    <?php
     echo $args['after_widget'];
   }
 
-  /**
-   * Back-end widget form.
-   *
-   * @see WP_Widget::form()
-   *
-   * @param array $instance Previously saved values from database.
-   */
   public function form( $instance ) {
     $title = ! empty( $instance['title'] ) ? $instance['title'] : '';
     $text = ! empty( $instance['text'] ) ? $instance['text'] : '';
     $button_text = ! empty( $instance['button_text'] ) ? $instance['button_text'] : '';
     $button_href = ! empty( $instance['button_href'] ) ? $instance['button_href'] : '';
-    $container_fluid = $instance[ 'container_fluid' ] ? 'true' : 'false';
     $css_class = ! empty( $instance['css_class'] ) ? $instance['css_class'] : '';
-    $button_css_class = ! empty( $instance['button_css_class'] ) ? $instance['button_css_class'] : 'btn-default';
+    $button_css_class = ! empty( $instance['button_css_class'] ) ? $instance['button_css_class'] : '';
     ?>
     <p>
     <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'understrap_widgets' ); ?></label> 
@@ -92,12 +65,6 @@ class UnderstrapCTA_Widget extends WP_Widget {
     </p>
 
     <p>
-    <input class="checkbox" type="checkbox" <?php checked( $instance[ 'container_fluid' ], 'on' ); ?> id="<?php echo $this->get_field_id( 'container_fluid' ); ?>" name="<?php echo $this->get_field_name( 'container_fluid' ); ?>" />
-    <label for="<?php echo $this->get_field_id( 'container_fluid' ); ?>"><?php _e( 'Is container-fluid?', 'understrap_widgets' ); ?></label><br>
-    <small><?php _e( 'Check to apply boostrap <code>container-fluid</code> class to this widget.', 'understrap_widgets' ); ?></small>
-    </p>
-
-    <p>
     <label for="<?php echo $this->get_field_id( 'css_class' ); ?>"><?php _e( 'Custom CSS Classes:', 'understrap_widgets' ); ?></label> 
     <input class="widefat" id="<?php echo $this->get_field_id( 'css_class' ); ?>" name="<?php echo $this->get_field_name( 'css_class' ); ?>" type="text" value="<?php esc_attr_e( $css_class ); ?>">
     </p>
@@ -109,24 +76,12 @@ class UnderstrapCTA_Widget extends WP_Widget {
     <?php 
   }
 
-  /**
-   * Sanitize widget form values as they are saved.
-   *
-   * @see WP_Widget::update()
-   *
-   * @param array $new_instance Values just sent to be saved.
-   * @param array $old_instance Previously saved values from database.
-   *
-   * @return array Updated safe values to be saved.
-   */
   public function update( $new_instance, $old_instance ) {
     $instance = array();
     $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
     $instance['text'] = ( ! empty( $new_instance['text'] ) ) ? wp_kses_post( $new_instance['text'] ) : '';
     $instance['button_text'] = ( ! empty( $new_instance['button_text'] ) ) ? sanitize_text_field( $new_instance['button_text'] ) : '';
-    $instance['button_href'] = ( ! empty( $new_instance['button_href'] ) ) ? esc_url_raw( $new_instance['button_href'] ) : ''; // url escaped for database insertion
-
-    $instance['container_fluid'] = $new_instance['container_fluid'];
+    $instance['button_href'] = ( ! empty( $new_instance['button_href'] ) ) ? esc_url_raw( $new_instance['button_href'] ) : '';
     // TODO: Sanitize css class names accordingly to w3c specifications
     $instance['css_class'] = ( ! empty( $new_instance['css_class'] ) ) ? sanitize_text_field( $new_instance['css_class'] ) : '';
     $instance['button_css_class'] = ( ! empty( $new_instance['button_css_class'] ) ) ? sanitize_text_field( $new_instance['button_css_class'] ) : '';
