a5eb4f0dce8ce522cd454d6488ea33bdd81fc300
Diego Versiani <diegoversiani@gmail.com>
2016-05-04 20:11:23 -0300

    Added About Widget title class.

--- widgets/understrap_about_widget.php
+++ widgets/understrap_about_widget.php
@@ -26,7 +26,7 @@ class UnderstrapAbout_Widget extends WP_Widget {
   public function widget( $args, $instance ) {
 
     if ( ! empty( $instance['title_tag'] ) ) {
-      $args['before_title'] = '<' . esc_attr( $instance['title_tag'] ) . '>';
+      $args['before_title'] = '<' . esc_attr( $instance['title_tag'] ) . ' class="about-title">';
       $args['after_title'] = '</' . esc_attr( $instance['title_tag'] ) . '>';
     }
 
