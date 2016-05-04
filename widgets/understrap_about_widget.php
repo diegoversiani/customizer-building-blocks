a9383fe85ba4d9e4d8483d2fed1bc198e6fefe85
Diego Versiani <diegoversiani@gmail.com>
2016-05-04 19:52:04 -0300

    About Widget improvements.

--- widgets/understrap_about_widget.php
+++ widgets/understrap_about_widget.php
@@ -24,43 +24,47 @@ class UnderstrapAbout_Widget extends WP_Widget {
    * @param array $instance Saved values from database.
    */
   public function widget( $args, $instance ) {
+
+    if ( ! empty( $instance['title_tag'] ) ) {
+      $args['before_title'] = '<' . esc_attr( $instance['title_tag'] ) . '>';
+      $args['after_title'] = '</' . esc_attr( $instance['title_tag'] ) . '>';
+    }
+
+    $widget_text = ! empty( $instance['text'] ) ? $instance['text'] : '';
+    $text_escaped = apply_filters( 'widget_text', $widget_text, $instance, $this );
+    $button_css_class = ( ! empty( $instance['button_css_class'] ) ) ? $instance['button_css_class'] : 'btn-secondary';
+
     echo $args['before_widget'];
 
-    $container_class = ( $instance['container_fluid'] ? 'container-fluid' : 'container' );
+    ?>
+    <div class="about <?php esc_attr_e( $instance['css_class'] ); ?>">
 
-    ?> 
-    <div class="wrapper wrapper-about <?php esc_attr_e( $instance['css_class'] ); ?>">
-      <div class="<?php esc_attr_e( $container_class ); ?>">
-        <div class="row">
-          <div class="col-xs-12 about-content">
-            <?php 
-            if ( ! empty( $instance['title'] ) ) {
-              echo $args['before_title'] . esc_html( $instance['title'] ) . $args['after_title'];
-            }
-            ?>
-            
-            <div class="about-text">
-              <?php echo wp_kses_post( $instance['text'] ); ?>
-            </div>
-
-            <?php if ( ! empty( $instance['cta_text'] ) || ! empty( $instance['button_text'] ) ) : ?>
-            <div class="about-cta">
-              <?php if ( ! empty( $instance['cta_text'] ) ) : ?>
-              <p><?php esc_html_e( $instance['cta_text'] ); ?></p>
-              <?php endif; ?>
-
-              <?php if ( ! empty( $instance['button_text'] ) ) : ?>
-              <a href="<?php esc_html_e( $instance['button_href'] ); ?>" class="btn <?php echo ( empty( $instance['button_css_class'] ) ? 'btn-default' : esc_attr( $instance['button_css_class'] ) ); ?>"><?php esc_html_e( $instance['button_text'] ); ?></a>
-              <?php endif; ?>
-            </div>
-            <?php endif; ?>
-
-          </div>
-        </div>
+      <?php 
+      if ( ! empty( $instance['title'] ) ) {
+        echo $args['before_title'] . esc_html( $instance['title'] ) . $args['after_title'];
+      }
+      ?>
+      
+      <div class="about-text">
+        <?php echo $text_escaped; ?>
       </div>
+
+      <?php if ( ! empty( $instance['cta_text'] ) || ! empty( $instance['button_text'] ) ) : ?>
+      <div class="about-cta">
+        <?php if ( ! empty( $instance['cta_text'] ) ) : ?>
+        <p><?php esc_html_e( $instance['cta_text'] ); ?></p>
+        <?php endif; ?>
+
+        <?php if ( ! empty( $instance['button_text'] ) ) : ?>
+        <a href="<?php echo esc_url( $instance['button_href'] ); ?>" class="btn <?php esc_attr_e( $button_css_class ); ?>"><?php esc_html_e( $instance['button_text'] ); ?></a>
+        <?php endif; ?>
+      </div>
+      <?php endif; ?>
+
     </div>
 
     <?php
+    echo $args['after_widget'];
   }
 
   /**
@@ -72,11 +76,11 @@ class UnderstrapAbout_Widget extends WP_Widget {
    */
   public function form( $instance ) {
     $title = ! empty( $instance['title'] ) ? $instance['title'] : '';
+    $title_tag = ! empty( $instance['title_tag'] ) ? $instance['title_tag'] : '';
     $text = ! empty( $instance['text'] ) ? $instance['text'] : '';
     $cta_text = ! empty( $instance['cta_text'] ) ? $instance['cta_text'] : '';
     $button_text = ! empty( $instance['button_text'] ) ? $instance['button_text'] : '';
     $button_href = ! empty( $instance['button_href'] ) ? $instance['button_href'] : '';
-    $container_fluid = $instance[ 'container_fluid' ] ? 'true' : 'false';
     $css_class = ! empty( $instance['css_class'] ) ? $instance['css_class'] : '';
     $button_css_class = ! empty( $instance['button_css_class'] ) ? $instance['button_css_class'] : '';
 
@@ -85,6 +89,21 @@ class UnderstrapAbout_Widget extends WP_Widget {
     <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'understrap_widgets' ); ?></label> 
     <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php esc_attr_e( $title ); ?>">
     </p>
+
+    <p>
+    <label><?php _e( 'Title Tags:', 'understrap_widgets' ); ?></label>
+    <br>
+    <select class="widefat" id="<?php echo $this->get_field_id( 'title_tag' ); ?>" name="<?php echo $this->get_field_name( 'title_tag' ); ?>">
+      <option value="" <?php esc_attr_e( $title_tag == '' ? 'selected' : '' ); ?>><?php _e( '-- None --', 'understrap_widgets' ); ?></option>
+      <option value="h1" <?php esc_attr_e( $title_tag == 'h1' ? 'selected' : '' ); ?>><?php _e( 'h1', 'understrap_widgets' ); ?></option>
+      <option value="h2" <?php esc_attr_e( $title_tag == 'h2' ? 'selected' : '' ); ?>><?php _e( 'h2', 'understrap_widgets' ); ?></option>
+      <option value="h3" <?php esc_attr_e( $title_tag == 'h3' ? 'selected' : '' ); ?>><?php _e( 'h3', 'understrap_widgets' ); ?></option>
+      <option value="h4" <?php esc_attr_e( $title_tag == 'h4' ? 'selected' : '' ); ?>><?php _e( 'h4', 'understrap_widgets' ); ?></option>
+      <option value="h5" <?php esc_attr_e( $title_tag == 'h5' ? 'selected' : '' ); ?>><?php _e( 'h5', 'understrap_widgets' ); ?></option>
+      <option value="h6" <?php esc_attr_e( $title_tag == 'h6' ? 'selected' : '' ); ?>><?php _e( 'h6', 'understrap_widgets' ); ?></option>
+      <option value="span" <?php esc_attr_e( $title_tag == 'span' ? 'selected' : '' ); ?>><?php _e( 'span', 'understrap_widgets' ); ?></option>
+    </select>
+    <small><?php _e( 'Apply only for the section title, not the widgets inside it.', 'understrap_widgets' ); ?></small>
 
     <p>
     <label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Text:', 'understrap_widgets' ); ?></label> 
@@ -104,12 +123,6 @@ class UnderstrapAbout_Widget extends WP_Widget {
     <p>
     <label for="<?php echo $this->get_field_id( 'button_href' ); ?>"><?php _e( 'Button Action:', 'understrap_widgets' ); ?></label> 
     <input class="widefat" id="<?php echo $this->get_field_id( 'button_href' ); ?>" name="<?php echo $this->get_field_name( 'button_href' ); ?>" type="text" value="<?php esc_attr_e( $button_href ); ?>">
-    </p>
-
-    <p>
-    <input class="checkbox" type="checkbox" <?php checked( $instance[ 'container_fluid' ], 'on' ); ?> id="<?php echo $this->get_field_id( 'container_fluid' ); ?>" name="<?php echo $this->get_field_name( 'container_fluid' ); ?>" />
-    <label for="<?php echo $this->get_field_id( 'container_fluid' ); ?>"><?php _e( 'Is container-fluid?', 'understrap_widgets' ); ?></label><br>
-    <small><?php _e( 'Check to apply boostrap <code>container-fluid</code> class to this widget.', 'understrap_widgets' ); ?></small>
     </p>
 
     <p>
@@ -137,12 +150,13 @@ class UnderstrapAbout_Widget extends WP_Widget {
   public function update( $new_instance, $old_instance ) {
     $instance = array();
     $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+    $title_tag_allowed = array('h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span');
+    $instance['title_tag'] = ( in_array($new_instance['title_tag'], $title_tag_allowed) ) ? $new_instance['title_tag'] : '';
     $instance['text'] = ( ! empty( $new_instance['text'] ) ) ? wp_kses_post( $new_instance['text'] ) : '';
     $instance['cta_text'] = ( ! empty( $new_instance['cta_text'] ) ) ? sanitize_text_field( $new_instance['cta_text'] ) : '';
     $instance['button_text'] = ( ! empty( $new_instance['button_text'] ) ) ? sanitize_text_field( $new_instance['button_text'] ) : '';
     $instance['button_href'] = ( ! empty( $new_instance['button_href'] ) ) ? esc_url_raw( $new_instance['button_href'] ) : ''; // url escaped for database insertion
 
-    $instance['container_fluid'] = $new_instance['container_fluid'];
     // TODO: Sanitize css class names accordingly to w3c specifications
     $instance['css_class'] = ( ! empty( $new_instance['css_class'] ) ) ? sanitize_text_field( $new_instance['css_class'] ) : '';
     $instance['button_css_class'] = ( ! empty( $new_instance['button_css_class'] ) ) ? sanitize_text_field( $new_instance['button_css_class'] ) : '';
