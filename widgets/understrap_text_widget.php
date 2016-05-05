596bad70179be4be715bfee6a8c6a5832853d6e3
Diego Versiani <diegoversiani@gmail.com>
2016-05-04 21:42:52 -0300

    Added title option to Text Widget.

--- widgets/understrap_text_widget.php
+++ widgets/understrap_text_widget.php
@@ -24,12 +24,18 @@ class UnderstrapText_Widget extends WP_Widget {
    * @param array $instance Saved values from database.
    */
   public function widget( $args, $instance ) {
-    echo $args['before_widget'];
+
+    if ( ! empty( $instance['title_tag'] ) ) {
+      $args['before_title'] = '<' . esc_attr( $instance['title_tag'] ) . ' class="text-title">';
+      $args['after_title'] = '</' . esc_attr( $instance['title_tag'] ) . '>';
+    }
 
     $container_class = ( $instance['container_fluid'] ? 'container-fluid' : 'container' );
 
     $widget_text = ! empty( $instance['text'] ) ? $instance['text'] : '';
     $text_escaped = apply_filters( 'widget_text', $widget_text, $instance, $this );
+    
+    echo $args['before_widget'];
 
     ?>
       <div class="<?php echo ( empty ( $instance['css_class'] ) ) ? 'col-xs-12' : esc_attr( $instance['css_class'] ); ?>">
@@ -57,6 +63,7 @@ class UnderstrapText_Widget extends WP_Widget {
    */
   public function form( $instance ) {
     $title = ! empty( $instance['title'] ) ? $instance['title'] : '';
+    $title_tag = ! empty( $instance['title_tag'] ) ? $instance['title_tag'] : '';
     $text = ! empty( $instance['text'] ) ? $instance['text'] : '';
     $css_class = ! empty( $instance['css_class'] ) ? $instance['css_class'] : '';
     ?>
@@ -65,6 +72,20 @@ class UnderstrapText_Widget extends WP_Widget {
     <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'understrap_widgets' ); ?></label> 
     <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php esc_attr_e( $title ); ?>">
     </p>
+
+    <p>
+    <label><?php _e( 'Title Tags:', 'understrap_widgets' ); ?></label>
+    <br>
+    <select class="widefat" id="<?php echo $this->get_field_id( 'title_tag' ); ?>" name="<?php echo $this->get_field_name( 'title_tag' ); ?>">
+      <option value="" <?php esc_attr_e( $title_tag == '' ? 'selected' : '' ); ?>><?php _e( '-- None --', 'understrap_widgets' ); ?></option>
+      <option value="h1" <?php esc_attr_e( $title_tag == 'h1' ? 'selected' : '' ); ?>><?php _e( 'h1', 'understrap_widgets' ); ?></option>
+      <option value="h2" <?php esc_attr_e( $title_tag == 'h2' ? 'selected' : '' ); ?>><?php _e( 'h2', 'understrap_widgets' ); ?></option>
+      <option value="h3" <?php esc_attr_e( $title_tag == 'h3' ? 'selected' : '' ); ?>><?php _e( 'h3', 'understrap_widgets' ); ?></option>
+      <option value="h4" <?php esc_attr_e( $title_tag == 'h4' ? 'selected' : '' ); ?>><?php _e( 'h4', 'understrap_widgets' ); ?></option>
+      <option value="h5" <?php esc_attr_e( $title_tag == 'h5' ? 'selected' : '' ); ?>><?php _e( 'h5', 'understrap_widgets' ); ?></option>
+      <option value="h6" <?php esc_attr_e( $title_tag == 'h6' ? 'selected' : '' ); ?>><?php _e( 'h6', 'understrap_widgets' ); ?></option>
+      <option value="span" <?php esc_attr_e( $title_tag == 'span' ? 'selected' : '' ); ?>><?php _e( 'span', 'understrap_widgets' ); ?></option>
+    </select>
 
     <p>
     <label for="<?php echo $this->get_field_id( 'text' ); ?>"><?php _e( 'Text:', 'understrap_widgets' ); ?></label> 
@@ -92,6 +113,8 @@ class UnderstrapText_Widget extends WP_Widget {
   public function update( $new_instance, $old_instance ) {
     $instance = array();
     $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+    $title_tag_allowed = array('h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span');
+    $instance['title_tag'] = ( in_array($new_instance['title_tag'], $title_tag_allowed) ) ? $new_instance['title_tag'] : '';
     $instance['text'] = ( ! empty( $new_instance['text'] ) ) ? wp_kses_post( $new_instance['text'] ) : '';
 
     // TODO: Sanitize css classes for allowed characters
