b869051ac7c5ca11c774442aeedfbec09628f8b3
diegoversiani <diegoversiani@gmail.com>
2016-08-01 20:02:05 -0300

    Fix social networks icon classes.

--- widget-templates/default/cbb_social_networks_item_template.php
+++ widget-templates/default/cbb_social_networks_item_template.php
@@ -5,8 +5,6 @@
  * in the folder `plugins/customizer-building-blocks/widget-templates` in your active theme
  */
 
-$social_network_classes = 'i-' . $social_network_classes;
-
 ?>
 
 <li class="cbb-social-networks__list-item"><a href="<?php echo esc_url( $instance[ $social_network_url ] ); ?>" target="_blank"><i class="<?php esc_attr_e( $social_network_classes ) ?>"></i></a></li>
